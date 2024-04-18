6bf63d642820d8e0a1e6113ba4cfd82c024ab53f
강민석 <bluekms@shiftup.co.kr>
2024-04-18 19:45:13 +0900

    ExcelColumnExtractor Project

---
+++ ExcelColumnExtractor/ProgramOptions.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace ExcelColumnExtractor;
+
+public sealed class ProgramOptions
+{
+    [Option('c', "class-path", Required = true, HelpText = "C# 클래스 파일 경로")]
+    public string ClassPath { get; set; } = null!;
+
+    [Option('e', "excel-path", Required = true, HelpText = "액셀 파일 경로")]
+    public string ExcelPath { get; set; } = null!;
+
+    [Option('o', "output", Required = false, HelpText = "출력 파일 경로")]
+    public string? OutputPath { get; set; }
+}
