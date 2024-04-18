691e481b0d6202387cd1fcfa476ad43b519a7b7b
강민석 <bluekms@shiftup.co.kr>
2024-04-18 19:44:23 +0900

    SchemaInfoScanner

---
+++ SchemaInfoScanner/TableInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace SchemaInfoScanner;
+
+public sealed record TableInfo(
+    string? SheetName,
+    string RecordName,
+    IReadOnlyList<ColumnInfo> ColumnInfoList)
+{
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+
+        if (SheetName is null)
+        {
+            sb.Append(nameof(RecordName));
+            sb.Append(": ");
+            sb.AppendLine(RecordName);
+        }
+        else
+        {
+            sb.Append(nameof(RecordName));
+            sb.Append(": ");
+            sb.Append(RecordName);
+            sb.Append('(');
+            sb.Append(SheetName);
+            sb.AppendLine(")");
+        }
+
+        foreach (var columnInfo in ColumnInfoList)
+        {
+            sb.Append(" - ");
+            sb.AppendLine(columnInfo.ToString());
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ SchemaInfoScanner/TypeChecker/HashSetTypeChecker.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SchemaInfoScanner.TypeChecker;
+
+public class HashSetTypeChecker
+{
+    public static bool IsHashSet(INamedTypeSymbol symbol)
+    {
+        return symbol.Name.StartsWith("HashSet", StringComparison.Ordinal);
+    }
+
+    public static bool CheckSupportedType(INamedTypeSymbol symbol)
+    {
+        if (!IsHashSet(symbol) ||
+            symbol.TypeArguments is not [INamedTypeSymbol namedTypeSymbol])
+        {
+            return false;
+        }
+
+        return PrimitiveTypeChecker.CheckSupportedType(namedTypeSymbol);
+    }
+}
