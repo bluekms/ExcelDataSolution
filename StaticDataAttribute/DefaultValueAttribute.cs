8f5ff68c8578de53c21d4ad7094c15f66758acdb
minseok.kang <bluekms21@naver.com>
2024-04-20 02:43:01 +0900

    NullString, DefaultValue 추가

---
+++ StaticDataAttribute/DefaultValueAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace StaticDataAttribute;
+
+[AttributeUsage(AttributeTargets.Parameter)]
+public class DefaultValueAttribute : Attribute
+{
+    public string DefaultValue { get; }
+
+    public DefaultValueAttribute(string defaultValue)
+    {
+        DefaultValue = defaultValue;
+    }
+}
