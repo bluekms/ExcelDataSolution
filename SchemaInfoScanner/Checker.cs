d68b0570f9b7173b370aed22355c5cbe93475bff
강민석 <bluekms@shiftup.co.kr>
2024-05-20 12:55:37 +0900

    Add TryCheck

--- SchemaInfoScanner/Checker.cs
+++ SchemaInfoScanner/Checker.cs
@@ -9,7 +9,10 @@ namespace SchemaInfoScanner;
 public static class Checker
 {
     private static readonly Action<ILogger, string, Exception?> LogTrace =
-        LoggerMessage.Define<string>(LogLevel.Trace, new EventId(0), "{Message}");
+        LoggerMessage.Define<string>(LogLevel.Trace, new EventId(0, nameof(LogTrace)), "{Message}");
+
+    private static readonly Action<ILogger, string, Exception?> LogException =
+        LoggerMessage.Define<string>(LogLevel.Error, new EventId(1, nameof(LogException)), "{Message}");
 
     public static void Check(RecordSchemaContainer recordSchemaContainer, SemanticModelContainer semanticModelContainer, ILogger logger)
     {
@@ -30,6 +33,36 @@ public static class Checker
             foreach (var recordParameter in recordSchema.RecordParameterSchemaList)
             {
                 SupportedTypeChecker.Check(recordParameter, recordSchemaContainer, semanticModelContainer, visited, logger);
+            }
+        }
+    }
+
+    public static void TryCheck(RecordSchemaContainer recordSchemaContainer, SemanticModelContainer semanticModelContainer, ILogger logger)
+    {
+        var visited = new HashSet<RecordName>();
+        foreach (var (_, recordSchema) in recordSchemaContainer.RecordSchemaDictionary)
+        {
+            if (!recordSchema.HasAttribute<StaticDataRecordAttribute>())
+            {
+                continue;
+            }
+
+            if (!visited.Add(recordSchema.RecordName))
+            {
+                LogTrace(logger, $"{recordSchema.RecordName.FullName} is already visited.", null);
+                continue;
+            }
+
+            foreach (var recordParameter in recordSchema.RecordParameterSchemaList)
+            {
+                try
+                {
+                    SupportedTypeChecker.Check(recordParameter, recordSchemaContainer, semanticModelContainer, visited, logger);
+                }
+                catch (Exception e)
+                {
+                    LogException(logger, $"{recordParameter.ParameterName.FullName}: {e.Message}", e);
+                }
             }
         }
     }
--- _TestRecord/FooRecord.cs
+++ _TestRecord/FooRecord.cs
@@ -4,4 +4,7 @@
 public sealed record ClassA(string Name, int Score, [ColumnPrefix("ClassA_")] List<ClassB> ClassBs, int Foo);
 
 [StaticDataRecord]
-public sealed record ClassB(string Name, float Score, [ColumnPrefix("ClassB_")] List<ClassA> ClassAs, string Foo, [ColumnPrefix("Err_")] HashSet<ClassA> Err, double Bar);
+public sealed record ClassB(string Name, float Score, [ColumnPrefix("ClassB_")] List<ClassA> ClassAs, [ColumnPrefix("Err_")] HashSet<ClassA> Err, string Foo, double Bar);
+
+[StaticDataRecord]
+public sealed record ClassC(string Name, float Score, [ColumnPrefix("Err_")] HashSet<ClassA> Err, string FooC, double BarC);
