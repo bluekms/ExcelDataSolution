20ae3086ae934fe0af4e0fff863e2463327a9309
강민석 <bluekms@shiftup.co.kr>
2024-05-24 18:28:48 +0900

    ProgramOptions 갱신 및 logger 적용

--- ExcelColumnExtractor/ProgramOptions.cs
+++ ExcelColumnExtractor/ProgramOptions.cs
@@ -1,4 +1,6 @@
 ﻿using CommandLine;
+using Microsoft.Extensions.Logging;
+using Serilog.Events;
 
 namespace ExcelColumnExtractor;
 
@@ -10,6 +12,12 @@ public sealed class ProgramOptions
     [Option('e', "excel-path", Required = true, HelpText = "액셀 파일 경로")]
     public string ExcelPath { get; set; } = null!;
 
-    [Option('o', "output", Required = false, HelpText = "출력 파일 경로")]
-    public string? OutputPath { get; set; }
+    [Option('o', "output-path", Required = true, HelpText = "출력 파일 경로")]
+    public string OutputPath { get; set; } = null!;
+
+    [Option('l', "log-path", Required = false, HelpText = "로그 파일 경로")]
+    public string? LogPath { get; set; }
+
+    [Option(Default = LogEventLevel.Information, Required = false, HelpText = "최소 로그 레벨 (기본값: Information)")]
+    public LogEventLevel MinLogLevel { get; set; }
 }
