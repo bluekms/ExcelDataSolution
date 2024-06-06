4f15056cacffbc9e617707a8fd30292d8e9862f1
minseok.kang <bluekms21@naver.com>
2024-06-06 23:30:09 +0900

    SingleColumnContainerAttribute 추가

---
+++ StaticDataAttribute/SingleColumnContainerAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace StaticDataAttribute;
+
+[AttributeUsage(AttributeTargets.Parameter)]
+public class SingleColumnContainerAttribute : Attribute
+{
+    public string Separator { get; }
+
+    public SingleColumnContainerAttribute(string separator)
+    {
+        Separator = separator;
+    }
+}
