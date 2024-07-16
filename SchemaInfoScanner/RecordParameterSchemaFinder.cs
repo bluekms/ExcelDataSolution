5e7e9bd7a107206e5e3c6c4ed78d08c4353a713a
강민석 <bluekms@shiftup.co.kr>
2024-07-16 18:39:21 +0900

    parameterSchema -> parameter로 변수명 수정

--- SchemaInfoScanner/RecordParameterSchemaFinder.cs
+++ SchemaInfoScanner/RecordParameterSchemaFinder.cs
@@ -12,7 +12,11 @@ public sealed record RecordParameterSchemaFindResult(
 
 public static class RecordParameterSchemaFinder
 {
-    public static List<RecordParameterSchemaFindResult> Find(this RecordSchema recordSchema, string fullName, RecordSchemaContainer recordSchemaContainer, RecordParameterSchema? parentParameterSchema = null)
+    public static List<RecordParameterSchemaFindResult> Find(
+        this RecordSchema recordSchema,
+        string fullName,
+        RecordSchemaContainer recordSchemaContainer,
+        RecordParameterSchema? parentParameterSchema = null)
     {
         var schema = recordSchema.RecordParameterSchemaList.SingleOrDefault(x => x.ParameterName.FullName == fullName);
         if (schema is not null)
@@ -21,32 +25,32 @@ public static class RecordParameterSchemaFinder
         }
 
         var results = new List<RecordParameterSchemaFindResult>();
-        foreach (var parameterSchema in recordSchema.RecordParameterSchemaList)
+        foreach (var parameter in recordSchema.RecordParameterSchemaList)
         {
-            if (PrimitiveTypeChecker.IsSupportedPrimitiveType(parameterSchema.NamedTypeSymbol) ||
-                ContainerTypeChecker.IsPrimitiveContainer(parameterSchema.NamedTypeSymbol))
+            if (PrimitiveTypeChecker.IsSupportedPrimitiveType(parameter.NamedTypeSymbol) ||
+                ContainerTypeChecker.IsPrimitiveContainer(parameter.NamedTypeSymbol))
             {
                 continue;
             }
 
-            if (ContainerTypeChecker.IsSupportedContainerType(parameterSchema.NamedTypeSymbol))
+            if (ContainerTypeChecker.IsSupportedContainerType(parameter.NamedTypeSymbol))
             {
-                var typeArgument = (INamedTypeSymbol)parameterSchema.NamedTypeSymbol.TypeArguments.Single();
+                var typeArgument = (INamedTypeSymbol)parameter.NamedTypeSymbol.TypeArguments.Single();
                 var innerRecordName = new RecordName(typeArgument);
                 if (recordSchemaContainer.RecordSchemaDictionary.TryGetValue(innerRecordName, out var innerRecordSchema))
                 {
-                    results.AddRange(innerRecordSchema.Find(fullName, recordSchemaContainer, parameterSchema));
+                    results.AddRange(innerRecordSchema.Find(fullName, recordSchemaContainer, parameter));
                 }
 
                 continue;
             }
 
-            if (RecordTypeChecker.IsSupportedRecordType(parameterSchema.NamedTypeSymbol))
+            if (RecordTypeChecker.IsSupportedRecordType(parameter.NamedTypeSymbol))
             {
-                var innerRecordName = new RecordName(parameterSchema.NamedTypeSymbol);
+                var innerRecordName = new RecordName(parameter.NamedTypeSymbol);
                 if (recordSchemaContainer.RecordSchemaDictionary.TryGetValue(innerRecordName, out var innerRecordSchema))
                 {
-                    results.AddRange(innerRecordSchema.Find(fullName, recordSchemaContainer, parameterSchema));
+                    results.AddRange(innerRecordSchema.Find(fullName, recordSchemaContainer, parameter));
                 }
 
                 continue;
