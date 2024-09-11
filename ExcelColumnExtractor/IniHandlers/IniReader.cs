958da568e95b72b9ef55c7e0cf071c23e7f2d970
강민석 <bluekms@shiftup.co.kr>
2024-09-11 15:34:21 +0900

    ini Reader에서 string이 아니라 RecordName을 받도록 수정

--- ExcelColumnExtractor/IniHandlers/IniReader.cs
+++ ExcelColumnExtractor/IniHandlers/IniReader.cs
@@ -1,39 +1,40 @@
+using System.Collections.Frozen;
 using System.Data;
 using IniParser;
+using SchemaInfoScanner.NameObjects;
 
 namespace ExcelColumnExtractor.IniHandlers;
 
+public sealed record IniFileResult(RecordName RecordName, FrozenDictionary<string, int> HeaderNameLengths);
+
 public static class IniReader
 {
-    public static Dictionary<string, Dictionary<string, int>> Read(string path, HashSet<RecordContainerInfo> recordContainerInfos)
+    public static Dictionary<RecordName, IniFileResult> Read(string path, RecordContainerInfo recordContainerInfo)
     {
         var parser = new FileIniDataParser();
+        var fileName = Path.Combine(path, $"{recordContainerInfo.RecordName}.ini");
+        var iniData = parser.ReadFile(fileName);
 
-        var results = new Dictionary<string, Dictionary<string, int>>();
-        foreach (var recordContainerInfo in recordContainerInfos)
+        var results = new Dictionary<RecordName, IniFileResult>();
+        foreach (var section in iniData.Sections)
         {
-            var fileName = Path.Combine(path, $"{recordContainerInfo.RecordName}.ini");
-            var iniData = parser.ReadFile(fileName);
-            foreach (var section in iniData.Sections)
+            var headerNameLengths = new Dictionary<string, int>();
+            foreach (var key in section.Keys)
             {
-                var headerNameLengths = new Dictionary<string, int>();
-                foreach (var key in section.Keys)
+                if (!int.TryParse(key.Value, out var value))
                 {
-                    if (!int.TryParse(key.Value, out var value))
-                    {
-                        throw new FormatException($"Value {key.Value} is not a valid integer.");
-                    }
-
-                    headerNameLengths[key.KeyName] = value;
+                    throw new FormatException($"Value {key.Value} is not a valid integer.");
                 }
 
-                if (headerNameLengths.Count != recordContainerInfo.LengthRequiredHeaderNames.Count)
-                {
-                    throw new DataException("The ini file and the record version are different. Please recreate the length ini file.");
-                }
-
-                results[section.SectionName] = headerNameLengths;
+                headerNameLengths[key.KeyName] = value;
             }
+
+            if (headerNameLengths.Count != recordContainerInfo.LengthRequiredHeaderNames.Count)
+            {
+                throw new DataException("The ini file and the record version are different. Please recreate the length ini file.");
+            }
+
+            results[recordContainerInfo.RecordName] = new(recordContainerInfo.RecordName, headerNameLengths.ToFrozenDictionary());
         }
 
         return results;
