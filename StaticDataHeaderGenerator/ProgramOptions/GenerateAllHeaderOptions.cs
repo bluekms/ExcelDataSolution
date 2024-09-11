48b857c4915b39595f0104d5dd9e671a3d40d4d6
강민석 <bluekms@shiftup.co.kr>
2024-09-11 15:35:30 +0900

    Separator 추가 및 Options 정리

--- StaticDataHeaderGenerator/ProgramOptions/GenerateAllHeaderOptions.cs
+++ StaticDataHeaderGenerator/ProgramOptions/GenerateAllHeaderOptions.cs
@@ -9,11 +9,14 @@ public class GenerateAllHeaderOptions
     [Option('r', "record-path", Required = true, HelpText = "C# 레코드 파일 경로")]
     public string RecordCsPath { get; set; } = null!;
 
-    [Option('l', "length-ini-path", Required = true, HelpText = "Length Template Ini 파일 경로")]
+    [Option('i', "length-ini-path", Required = true, HelpText = "Length Template Ini 파일 경로")]
     public string LengthIniPath { get; set; } = null!;
 
-    [Option('o', "output-path", Required = true, HelpText = "Header Name 파일 경로")]
-    public string OutputPath { get; set; } = null!;
+    [Option('s', "separator", Default = "\t", Required = false, HelpText = "Header 구분자. 기본값: Tab")]
+    public string Separator { get; set; } = null!;
+
+    [Option('o', "output-file", Required = false, HelpText = "Header Name 파일 출력 경로. 없다면 콘솔에 출력")]
+    public string OutputFileName { get; set; } = null!;
 
     [Option('l', "log-path", Required = false, HelpText = "로그 파일 경로")]
     public string? LogPath { get; set; }
