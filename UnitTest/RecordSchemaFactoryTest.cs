d8f92fd0017830daf029768ebdf333f1b682901c
강민석 <bluekms@shiftup.co.kr>
2024-11-19 18:08:30 +0900

    UnitTest 적용

--- UnitTest/RecordSchemaFactoryTest.cs
+++ UnitTest/RecordSchemaFactoryTest.cs
@@ -45,6 +45,45 @@ public class RecordSchemaFactoryTest(ITestOutputHelper testOutputHelper)
         var logger = factory.CreateLogger<RecordScanTest>();
 
         var code = $$"""
+                     [StaticDataRecord("TestExcel", "TestSheet")]
+                     public sealed record MyRecord(
+                        {{type}} Parameter
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var enumMemberContainer = new EnumMemberContainer(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector, enumMemberContainer);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        var rawRecordSchema = recordSchemaContainer.RecordSchemaDictionary.Values.First();
+        var recordSchema = RecordSchemaFactory.Create(
+            rawRecordSchema,
+            recordSchemaContainer,
+            enumMemberContainer,
+            new Dictionary<string, int>());
+
+        for (var i = 0; i < 10; ++i)
+        {
+            var parameter = recordSchema.RecordParameterSchemaList[0];
+            var value = RandomValueGenerator.Generate(TypeConverter.GetSystemTypeName(type));
+            var valueStr = value?.ToString() ?? string.Empty;
+
+            parameter.CheckCompatibility(valueStr, logger);
+        }
+    }
+
+    [Theory]
+    [InlineData("MyEnum")]
+    [InlineData("MyEnum?")]
+    public void EnumTypeTest(string type)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Trace);
+        var logger = factory.CreateLogger<RecordScanTest>();
+
+        var code = $$"""
                      public enum MyEnum { A, B, C }
 
                      [StaticDataRecord("TestExcel", "TestSheet")]
@@ -67,14 +106,119 @@ public class RecordSchemaFactoryTest(ITestOutputHelper testOutputHelper)
             enumMemberContainer,
             new Dictionary<string, int>());
 
-        var first = recordSchema.RecordParameterSchemaList[0];
-        var value = RandomValueGenerator.Generate(TypeConverter.GetSystemTypeName(type));
-        var valueStr = value.ToString()!;
+        var valueStr = "A";
 
-        first.CheckCompatibility(valueStr, logger);
+        var parameter = recordSchema.RecordParameterSchemaList[0];
+        parameter.CheckCompatibility(valueStr, logger);
         if (type.EndsWith('?'))
         {
-            first.CheckCompatibility(string.Empty, logger);
+            parameter.CheckCompatibility(string.Empty, logger);
         }
+    }
+
+    [Theory]
+    [InlineData("bool")]
+    [InlineData("byte")]
+    [InlineData("char")]
+    [InlineData("decimal")]
+    [InlineData("double")]
+    [InlineData("float")]
+    [InlineData("int")]
+    [InlineData("long")]
+    [InlineData("sbyte")]
+    [InlineData("short")]
+    [InlineData("string")]
+    [InlineData("uint")]
+    [InlineData("ulong")]
+    [InlineData("ushort")]
+    [InlineData("bool?")]
+    [InlineData("byte?")]
+    [InlineData("char?")]
+    [InlineData("decimal?")]
+    [InlineData("double?")]
+    [InlineData("float?")]
+    [InlineData("int?")]
+    [InlineData("long?")]
+    [InlineData("sbyte?")]
+    [InlineData("short?")]
+    [InlineData("string?")]
+    [InlineData("uint?")]
+    [InlineData("ulong?")]
+    [InlineData("ushort?")]
+    public void SingleColumnListTest(string type)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Trace);
+        var logger = factory.CreateLogger<RecordScanTest>();
+
+        var code = $$"""
+                     [StaticDataRecord("TestExcel", "TestSheet")]
+                     public sealed record MyRecord(
+                        [SingleColumnContainer(",")]
+                        List<{{type}}> Parameter
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var enumMemberContainer = new EnumMemberContainer(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector, enumMemberContainer);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        var rawRecordSchema = recordSchemaContainer.RecordSchemaDictionary.Values.First();
+        var recordSchema = RecordSchemaFactory.Create(
+            rawRecordSchema,
+            recordSchemaContainer,
+            enumMemberContainer,
+            new Dictionary<string, int>());
+
+        var count = Random.Shared.Next(2, 10);
+        var list = Enumerable.Repeat(
+            RandomValueGenerator.Generate(TypeConverter.GetSystemTypeName(type))?.ToString() ?? string.Empty,
+            count);
+        var argument = string.Join(", ", list);
+
+        var parameter = recordSchema.RecordParameterSchemaList[0];
+        parameter.CheckCompatibility(argument, logger);
+    }
+
+    [Theory]
+    [InlineData("MyEnum")]
+    [InlineData("MyEnum?")]
+    public void SingleColumnEnumListTest(string type)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Trace);
+        var logger = factory.CreateLogger<RecordScanTest>();
+
+        var code = $$"""
+                     [StaticDataRecord("TestExcel", "TestSheet")]
+                     public sealed record MyRecord(
+                        [SingleColumnContainer(",")]
+                        List<{{type}}> Parameter
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var enumMemberContainer = new EnumMemberContainer(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector, enumMemberContainer);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        var rawRecordSchema = recordSchemaContainer.RecordSchemaDictionary.Values.First();
+        var recordSchema = RecordSchemaFactory.Create(
+            rawRecordSchema,
+            recordSchemaContainer,
+            enumMemberContainer,
+            new Dictionary<string, int>());
+
+        var count = Random.Shared.Next(2, 10);
+        var list = Enumerable.Repeat(
+            RandomValueGenerator.Generate(TypeConverter.GetSystemTypeName(type))?.ToString() ?? string.Empty,
+            count);
+        var argument = string.Join(", ", list);
+
+        var parameter = recordSchema.RecordParameterSchemaList[0];
+        parameter.CheckCompatibility(argument, logger);
     }
 }
