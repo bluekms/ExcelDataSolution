6e8c32f8134155125648ab17a5c443ab1fe6d777
강민석 <bluekms@shiftup.co.kr>
2024-12-18 12:20:25 +0900

    Add Analyzers for xunit test logger

---
+++ Analyzers/Analyzers/XunitTestLoggerAnalyzer.cs
@@ -0,0 +1,80 @@
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Analyzers;
+
+#pragma warning disable RS1038
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+#pragma warning restore RS1038
+public class XunitTestLoggerTypeAnalyzer : DiagnosticAnalyzer
+{
+    private const string DiagnosticId = "EDS1001";
+
+    private static readonly DiagnosticDescriptor Rule = new(
+        DiagnosticId,
+        "Generic type name mismatch",
+        "Generic type name '{0}' does not match class name '{1}'",
+        "Naming",
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+        context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.MethodDeclaration);
+    }
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+    private static void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+    {
+        var methodDeclaration = (MethodDeclarationSyntax)context.Node;
+
+        var hasFactOrTheoryAttribute = methodDeclaration.AttributeLists
+            .SelectMany(a => a.Attributes)
+            .Any(a => a.Name.ToString() == "Fact" || a.Name.ToString() == "Theory");
+
+        if (!hasFactOrTheoryAttribute)
+        {
+            return;
+        }
+
+        var classDeclaration = methodDeclaration.Ancestors().OfType<ClassDeclarationSyntax>().FirstOrDefault();
+        if (classDeclaration == null)
+        {
+            return;
+        }
+
+        var className = classDeclaration.Identifier.Text;
+        var loggerVariable = methodDeclaration.DescendantNodes()
+            .OfType<DeclarationPatternSyntax>()
+            .FirstOrDefault(v => v.Type.ToString().StartsWith("TestOutputLogger"));
+        if (loggerVariable is null)
+        {
+            return;
+        }
+
+        var genericTypeName = ExtractGenericTypeArgument(loggerVariable.Type.ToString());
+        if (genericTypeName is null)
+        {
+            return;
+        }
+
+        if (genericTypeName != className)
+        {
+            var diagnostic = Diagnostic.Create(Rule, loggerVariable.GetLocation(), genericTypeName, className);
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+
+    private static string? ExtractGenericTypeArgument(string typeName)
+    {
+        var match = Regex.Match(typeName, @"<(.+)>");
+        return match.Success ? match.Groups[1].Value : null;
+    }
+}
---
+++ Analyzers/Analyzers/XunitTestLoggerLogsAnalyzer.cs
@@ -0,0 +1,63 @@
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Analyzers;
+
+#pragma warning disable RS1038
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+#pragma warning restore RS1038
+public class XunitTestLoggerLogsAnalyzer : DiagnosticAnalyzer
+{
+    private const string DiagnosticId = "EDS1002";
+
+    private static readonly DiagnosticDescriptor Rule = new(
+        DiagnosticId,
+        "Logger Logs Verification Required",
+        "If you declared a logger, you must check logger.Logs",
+        "Usage",
+        defaultSeverity: DiagnosticSeverity.Warning,
+        isEnabledByDefault: true);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+        context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.MethodDeclaration);
+    }
+
+    private void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+    {
+        var methodDeclaration = (MethodDeclarationSyntax)context.Node;
+
+        var hasFactOrTheoryAttribute = methodDeclaration.AttributeLists
+            .SelectMany(a => a.Attributes)
+            .Any(a => a.Name.ToString() == "Fact" || a.Name.ToString() == "Theory");
+        if (!hasFactOrTheoryAttribute)
+        {
+            return;
+        }
+
+        var loggerVariable = methodDeclaration.DescendantNodes()
+            .OfType<DeclarationPatternSyntax>()
+            .FirstOrDefault(v => v.Type.ToString().StartsWith("TestOutputLogger"));
+        if (loggerVariable is null)
+        {
+            return;
+        }
+
+        var logsUsage = methodDeclaration.DescendantNodes()
+            .OfType<InvocationExpressionSyntax>()
+            .Any(invocation => invocation.ArgumentList.Arguments.Any(x => x.ToString().Contains(".Logs")));
+
+        if (!logsUsage)
+        {
+            var diagnostic = Diagnostic.Create(Rule, loggerVariable.GetLocation(), loggerVariable.Type.ToString());
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
