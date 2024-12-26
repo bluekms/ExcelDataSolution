8f23ac26814922dbf18ce61287ec3506bd5ab3d7
강민석 <bluekms@shiftup.co.kr>
2024-12-26 16:36:25 +0900

    해봤음

--- StudyTreeCollections/Program.cs
+++ StudyTreeCollections/Program.cs
@@ -8,43 +8,81 @@ internal class Program
     {
         // SerialTreeNode();
         // EntityTreeNode();
-        UseQueryForEntityTreeNode();
+        // UseQueryForEntityTreeNode();
+        // ErrorDetection();
+        MutableTrees();
     }
 
-    // https://github.com/davidwest/TreeCollections/wiki/Entity-Definitions
-    private static void EntityDefinitions()
+    private static void TestDetachAttach()
     {
-        
+        var source = GetSource();
+        var rootItem = new Category((int)source.CategoryId, source.Name);
+        var root = new MutableEntityTreeNode<int, Category>(c => c.CategoryId, rootItem);
+        root.Build(source, dataNode => new Category((int)dataNode.CategoryId, dataNode.Name));
+
+        var target = root[3];
+        Console.WriteLine(target.IsRoot);
+        target.Detach();
+        Console.WriteLine(target.IsRoot);
+        Console.WriteLine(target.Children.Count);
+
+        target.OrderChildren();
+    }
+
+    private static void MutableTrees()
+    {
+        var source = GetSource();
+        var rootItem = new Category((int)source.CategoryId, source.Name);
+        var root = new MutableEntityTreeNode<int, Category>(c => c.CategoryId, rootItem);
+        root.Build(source, dataNode => new Category((int)dataNode.CategoryId, dataNode.Name));
+
+        var target = root[3];
+        target.AddChild(new Category(999, "AAA"));
+        target.AddChild(new Category(998, "BBB"), 1);
+        var list = target.Children.Select(x => x.Item.ToString()).ToList();
+        list.ForEach(x => Console.WriteLine(x.ToString()));
+        list.ForEach(x => Console.WriteLine(x.ToString()));
+    }
+
+    private static void ErrorDetection()
+    {
+        var source = GetSource();
+        var rootItem = new Category((int)source.CategoryId, source.Name);
+        var root = new ReadOnlyEntityTreeNode<int, Category>(c => c.CategoryId, rootItem, ErrorCheckOptions.Default);
+        root.Build(source, dataNode => new Category((int)dataNode.CategoryId, dataNode.Name));
+
+        var errorNodes = root
+            .Where(n => n.Error != IdentityError.None);
+
+        var siblingIdDuplicates = root
+            .Where(n => n.Error.HasFlag(IdentityError.SiblingIdDuplicate))
+            .Select(n => n.Item.ToString())
+            .ToList();
+
+        var siblingAliasDuplicates = root
+            .Where(n => n.Error.HasFlag(IdentityError.SiblingAliasDuplicate))
+            .Select(n => n.Item.ToString())
+            .ToList();
+
+        var cyclicIdDuplicates = root
+            .Where(n => n.Error.HasFlag(IdentityError.CyclicIdDuplicate))
+            .Select(n => n.Item.ToString())
+            .ToList();
+
+        var treeScopeIdDuplicates = root
+            .Where(n => n.Error.HasFlag(IdentityError.TreeScopeIdDuplicate))
+            .Select(n => n.Item.ToString())
+            .ToList();
+
+        Console.WriteLine(siblingIdDuplicates.Count);
+        Console.WriteLine(siblingAliasDuplicates.Count);
+        Console.WriteLine(cyclicIdDuplicates.Count);  // 2 (1, root), (1, Smells) : Default로 하면 0개
+        Console.WriteLine(treeScopeIdDuplicates.Count);   // 2 (4, Celebrities), (4, Philosophies) : Default로 해도 2개
     }
 
     private static void UseQueryForEntityTreeNode()
     {
-        var source =
-            new CategoryDataNode(1, "Root",
-                new CategoryDataNode(2, "Nouns",
-                    new CategoryDataNode(3, "People",
-                        new CategoryDataNode(4, "Celebrities"),
-                        new CategoryDataNode(5, "Bad Guys")),
-                    new CategoryDataNode(6, "Places",
-                        new CategoryDataNode(13, "Local"),
-                        new CategoryDataNode(14, "National"),
-                        new CategoryDataNode(15, "International")),
-                    new CategoryDataNode(7, "Things",
-                        new CategoryDataNode(16, "Concrete",
-                            new CategoryDataNode(21, "Everyday Objects"),
-                            new CategoryDataNode(22, "Tools")),
-                        new CategoryDataNode(17, "Abstract",
-                            new CategoryDataNode(4, "Philosophies"),
-                            new CategoryDataNode(19, "Math")))),
-                new CategoryDataNode(8, "Verbs",
-                    new CategoryDataNode(10, "Active"),
-                    new CategoryDataNode(11, "Passive")),
-                new CategoryDataNode(9, "Adjectives",
-                    new CategoryDataNode(12, "Sensory",
-                        new CategoryDataNode(23, "Colors"),
-                        new CategoryDataNode(1, "Smells")),
-                    new CategoryDataNode(20, "Sizes")));
-
+        var source = GetSource();
         var rootItem = new Category((int)source.CategoryId, source.Name);
         var root = new ReadOnlyEntityTreeNode<int, Category>(c => c.CategoryId, rootItem);
         root.Build(source, dataNode => new Category((int)dataNode.CategoryId, dataNode.Name));
@@ -81,32 +119,7 @@ internal class Program
 
     private static void EntityTreeNode()
     {
-        var source =
-            new CategoryDataNode(1, "Root",
-                new CategoryDataNode(2, "Nouns",
-                    new CategoryDataNode(3, "People",
-                        new CategoryDataNode(4, "Celebrities"),
-                        new CategoryDataNode(5, "Bad Guys")),
-                    new CategoryDataNode(6, "Places",
-                        new CategoryDataNode(13, "Local"),
-                        new CategoryDataNode(14, "National"),
-                        new CategoryDataNode(15, "International")),
-                    new CategoryDataNode(7, "Things",
-                        new CategoryDataNode(16, "Concrete",
-                            new CategoryDataNode(21, "Everyday Objects"),
-                            new CategoryDataNode(22, "Tools")),
-                        new CategoryDataNode(17, "Abstract",
-                            new CategoryDataNode(4, "Philosophies"),
-                            new CategoryDataNode(19, "Math")))),
-                new CategoryDataNode(8, "Verbs",
-                    new CategoryDataNode(10, "Active"),
-                    new CategoryDataNode(11, "Passive")),
-                new CategoryDataNode(9, "Adjectives",
-                    new CategoryDataNode(12, "Sensory",
-                        new CategoryDataNode(23, "Colors"),
-                        new CategoryDataNode(1, "Smells")),
-                    new CategoryDataNode(20, "Sizes")));
-
+        var source = GetSource();
         var rootItem = new Category((int)source.CategoryId, source.Name);
         var root = new ReadOnlyEntityTreeNode<int, Category>(c => c.CategoryId, rootItem);
         root.Build(source, dataNode => new Category((int)dataNode.CategoryId, dataNode.Name));
@@ -116,37 +129,41 @@ internal class Program
 
     private static void SerialTreeNode()
     {
-        var root =
-            new CategoryDataNode(1, "Root",
-                new CategoryDataNode(2, "Nouns",
-                    new CategoryDataNode(3, "People",
-                        new CategoryDataNode(4, "Celebrities"),
-                        new CategoryDataNode(5, "Bad Guys")),
-                    new CategoryDataNode(6, "Places",
-                        new CategoryDataNode(13, "Local"),
-                        new CategoryDataNode(14, "National"),
-                        new CategoryDataNode(15, "International")),
-                    new CategoryDataNode(7, "Things",
-                        new CategoryDataNode(16, "Concrete",
-                            new CategoryDataNode(21, "Everyday Objects"),
-                            new CategoryDataNode(22, "Tools")),
-                        new CategoryDataNode(17, "Abstract",
-                            new CategoryDataNode(4, "Philosophies"),
-                            new CategoryDataNode(19, "Math")))),
-                new CategoryDataNode(8, "Verbs",
-                    new CategoryDataNode(10, "Active"),
-                    new CategoryDataNode(11, "Passive")),
-                new CategoryDataNode(9, "Adjectives",
-                    new CategoryDataNode(12, "Sensory",
-                        new CategoryDataNode(23, "Colors"),
-                        new CategoryDataNode(1, "Smells")),
-                    new CategoryDataNode(20, "Sizes")));
+        var source = GetSource();
 
-        var allNodes = root.SelectAll();
-        var sCategories = root.Where(n => n.Name.StartsWith('S'));
-        var thingsRoot = root.FirstOrDefault(n => n.Name == "Things");
-        var things = root.FirstOrDefault(n => n.Name == "Things").SelectAll();
-        Console.WriteLine(root.ToString(n => n.ToString()));
+        var allNodes = source.SelectAll();
+        var sCategories = source.Where(n => n.Name.StartsWith('S'));
+        var thingsRoot = source.FirstOrDefault(n => n.Name == "Things");
+        var things = source.FirstOrDefault(n => n.Name == "Things").SelectAll();
+        Console.WriteLine(source.ToString(n => n.ToString()));
         //root.ForEach((n, i) => Console.WriteLine($@"level: {i}  {n}"));
+    }
+
+    private static CategoryDataNode GetSource()
+    {
+        return new CategoryDataNode(1, "Root",
+            new CategoryDataNode(2, "Nouns",
+                new CategoryDataNode(3, "People",
+                    new CategoryDataNode(4, "Celebrities"),
+                    new CategoryDataNode(5, "Bad Guys")),
+                new CategoryDataNode(6, "Places",
+                    new CategoryDataNode(13, "Local"),
+                    new CategoryDataNode(14, "National"),
+                    new CategoryDataNode(15, "International")),
+                new CategoryDataNode(7, "Things",
+                    new CategoryDataNode(16, "Concrete",
+                        new CategoryDataNode(21, "Everyday Objects"),
+                        new CategoryDataNode(22, "Tools")),
+                    new CategoryDataNode(17, "Abstract",
+                        new CategoryDataNode(4, "Philosophies"),
+                        new CategoryDataNode(19, "Math")))),
+            new CategoryDataNode(8, "Verbs",
+                new CategoryDataNode(10, "Active"),
+                new CategoryDataNode(11, "Passive")),
+            new CategoryDataNode(9, "Adjectives",
+                new CategoryDataNode(12, "Sensory",
+                    new CategoryDataNode(23, "Colors"),
+                    new CategoryDataNode(1, "Smells")),
+                new CategoryDataNode(20, "Sizes")));
     }
 }
