3ca028c189cb3faadac8564fcb31f133bfb4269f
강민석 <bluekms@shiftup.co.kr>
2024-12-30 16:34:00 +0900

    add IniWriters

--- StaticDataHeaderGenerator/IniHandlers/IniOverwriteWriter.cs
+++ StaticDataHeaderGenerator/IniHandlers/IniOverwriteWriter.cs
@@ -1,10 +1,9 @@
-using SchemaInfoScanner.NameObjects;
+using IniParser;
+using IniParser.Model;
 
 namespace StaticDataHeaderGenerator.IniHandlers;
 
-public sealed record RecordContainerInfo(RecordName RecordName, HashSet<string> LengthRequiredHeaderNames);
-
-public static class IniWriter
+public static class IniOverwriteWriter
 {
     public static void Write(string path, HashSet<RecordContainerInfo> recordContainerInfos)
     {
@@ -13,17 +12,17 @@ public static class IniWriter
             Directory.CreateDirectory(path);
         }
 
-        var parser = new IniParser.FileIniDataParser();
+        var parser = new FileIniDataParser();
 
         foreach (var recordContainerInfo in recordContainerInfos)
         {
-            var section = new IniParser.Model.SectionData(recordContainerInfo.RecordName.FullName);
+            var section = new SectionData(recordContainerInfo.RecordName.FullName);
             foreach (var headerName in recordContainerInfo.LengthRequiredHeaderNames)
             {
                 section.Keys.AddKey(headerName);
             }
 
-            var iniData = new IniParser.Model.IniData();
+            var iniData = new IniData();
             iniData.Sections.Add(section);
 
             var fileName = Path.Combine(path, $"{recordContainerInfo.RecordName.FullName}.ini");
