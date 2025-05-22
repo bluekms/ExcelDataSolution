09f8bf39a891b5a3f2222c4e049db1c6fe759992
강민석 <bluekms@shiftup.co.kr>
2025-05-22 20:06:06 +0900

    정리된 UnitTests

---
+++ UnitTest/TypedPropertySchemaTests/ContainerPropertySchemaTests/DictionaryTypeTests.cs
@@ -0,0 +1,410 @@
+using Microsoft.Extensions.Logging;
+using SchemaInfoScanner;
+using SchemaInfoScanner.Collectors;
+using SchemaInfoScanner.Containers;
+using UnitTest.Utility;
+using Xunit.Abstractions;
+
+namespace UnitTest.TypedPropertySchemaTests.ContainerPropertySchemaTests;
+
+public class DictionaryTypeTests(ITestOutputHelper testOutputHelper)
+{
+    [Theory]
+    [InlineData("bool")]
+    [InlineData("byte")]
+    [InlineData("char")]
+    [InlineData("decimal")]
+    [InlineData("double")]
+    [InlineData("float")]
+    [InlineData("int")]
+    [InlineData("long")]
+    [InlineData("sbyte")]
+    [InlineData("short")]
+    [InlineData("string")]
+    [InlineData("uint")]
+    [InlineData("ulong")]
+    [InlineData("ushort")]
+    public void DictionaryKeyTest(string key)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<DictionaryTypeTests>() is not TestOutputLogger<DictionaryTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         Dictionary<{{key}}, string> Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("MyEnum")]
+    public void DictionaryEnumKeyTest(string key)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<DictionaryTypeTests>() is not TestOutputLogger<DictionaryTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     public enum MyEnum { A, B, C }
+
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         Dictionary<{{key}}, string> Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("DateTime")]
+    public void DictionaryDateTimeKeyTest(string key)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<DictionaryTypeTests>() is not TestOutputLogger<DictionaryTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         [DateTimeFormat("yyyy-MM-dd HH:mm:ss.fff")]
+                         Dictionary<{{key}}, string> Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("TimeSpan")]
+    public void DictionaryTimeSpanKeyTest(string key)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<DictionaryTypeTests>() is not TestOutputLogger<DictionaryTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         [TimeSpanFormat("c")]
+                         Dictionary<{{key}}, string> Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("bool")]
+    [InlineData("byte")]
+    [InlineData("char")]
+    [InlineData("decimal")]
+    [InlineData("double")]
+    [InlineData("float")]
+    [InlineData("int")]
+    [InlineData("long")]
+    [InlineData("sbyte")]
+    [InlineData("short")]
+    [InlineData("string")]
+    [InlineData("uint")]
+    [InlineData("ulong")]
+    [InlineData("ushort")]
+    [InlineData("bool?")]
+    [InlineData("byte?")]
+    [InlineData("char?")]
+    [InlineData("decimal?")]
+    [InlineData("double?")]
+    [InlineData("float?")]
+    [InlineData("int?")]
+    [InlineData("long?")]
+    [InlineData("sbyte?")]
+    [InlineData("short?")]
+    [InlineData("string?")]
+    [InlineData("uint?")]
+    [InlineData("ulong?")]
+    [InlineData("ushort?")]
+    public void DictionaryValueTest(string value)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<DictionaryTypeTests>() is not TestOutputLogger<DictionaryTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         Dictionary<int, {{value}}> Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("MyEnum")]
+    [InlineData("MyEnum?")]
+    public void DictionaryEnumValueTest(string value)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<DictionaryTypeTests>() is not TestOutputLogger<DictionaryTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     public enum MyEnum { A, B, C }
+
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         Dictionary<int, {{value}}> Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("DateTime")]
+    [InlineData("DateTime?")]
+    public void DictionaryDateTimeValueTest(string value)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<DictionaryTypeTests>() is not TestOutputLogger<DictionaryTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         [DateTimeFormat("yyyy-MM-dd HH:mm:ss.fff")]
+                         Dictionary<int, {{value}}> Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("TimeSpan")]
+    [InlineData("TimeSpan?")]
+    public void DictionaryTimeSpanValueTest(string value)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<DictionaryTypeTests>() is not TestOutputLogger<DictionaryTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         [TimeSpanFormat("c")]
+                         Dictionary<int, {{value}}> Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("ValueEnum")]
+    [InlineData("ValueEnum?")]
+    public void DictionaryEnumKeyEnumValueTest(string value)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<DictionaryTypeTests>() is not TestOutputLogger<DictionaryTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     public enum KeyEnum { A, B, C }
+                     public enum ValueEnum { A, B, C }
+
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         Dictionary<KeyEnum, {{value}}> Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("DateTime")]
+    public void DictionaryDateTimeKeyAndValueTest(string type)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<DictionaryTypeTests>() is not TestOutputLogger<DictionaryTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         [DateTimeFormat("yyyy-MM-dd HH:mm:ss.fff")]
+                         Dictionary<{{type}}, {{type}}> Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("DateTime")]
+    public void DictionaryDateTimeKeyDateTimeValueTest(string type)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<DictionaryTypeTests>() is not TestOutputLogger<DictionaryTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         [DateTimeFormat("yyyy-MM-dd")]
+                         [DateTimeFormat("HH:mm:ss.fff")]
+                         Dictionary<{{type}}, {{type}}> Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("TimeSpan")]
+    public void DictionaryTimeSpanKeyAndValueTest(string type)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<DictionaryTypeTests>() is not TestOutputLogger<DictionaryTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         [TimeSpanFormat("c")]
+                         Dictionary<{{type}}, {{type}}> Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("TimeSpan")]
+    public void DictionaryTimeSpanKeyTimeSpanValueTest(string type)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<DictionaryTypeTests>() is not TestOutputLogger<DictionaryTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         [TimeSpanFormat("c")]
+                         [TimeSpanFormat("G")]
+                         Dictionary<{{type}}, {{type}}> Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+}
---
+++ UnitTest/TypedPropertySchemaTests/PrimitiveTypeTests.cs
@@ -0,0 +1,149 @@
+using Microsoft.Extensions.Logging;
+using SchemaInfoScanner;
+using SchemaInfoScanner.Collectors;
+using SchemaInfoScanner.Containers;
+using UnitTest.Utility;
+using Xunit.Abstractions;
+
+namespace UnitTest.TypedPropertySchemaTests;
+
+public class PrimitiveTypeTests(ITestOutputHelper testOutputHelper)
+{
+    [Theory]
+    [InlineData("bool")]
+    [InlineData("byte")]
+    [InlineData("char")]
+    [InlineData("decimal")]
+    [InlineData("double")]
+    [InlineData("float")]
+    [InlineData("int")]
+    [InlineData("long")]
+    [InlineData("sbyte")]
+    [InlineData("short")]
+    [InlineData("string")]
+    [InlineData("uint")]
+    [InlineData("ulong")]
+    [InlineData("ushort")]
+    [InlineData("bool?")]
+    [InlineData("byte?")]
+    [InlineData("char?")]
+    [InlineData("decimal?")]
+    [InlineData("double?")]
+    [InlineData("float?")]
+    [InlineData("int?")]
+    [InlineData("long?")]
+    [InlineData("sbyte?")]
+    [InlineData("short?")]
+    [InlineData("string?")]
+    [InlineData("uint?")]
+    [InlineData("ulong?")]
+    [InlineData("ushort?")]
+    public void PrimitiveTest(string type)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<PrimitiveTypeTests>() is not TestOutputLogger<PrimitiveTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                   [StaticDataRecord("Test", "TestSheet")]
+                   public sealed record MyRecord(
+                       {{type}} Property,
+                   );
+                   """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("MyEnum")]
+    [InlineData("MyEnum?")]
+    public void EnumTest(string type)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<PrimitiveTypeTests>() is not TestOutputLogger<PrimitiveTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     public enum MyEnum { A, B, C }
+
+                     [StaticDataRecord("TestExcel", "TestSheet")]
+                     public sealed record MyRecord(
+                        {{type}} Property
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("DateTime")]
+    [InlineData("DateTime?")]
+    public void DateTimeTest(string type)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<PrimitiveTypeTests>() is not TestOutputLogger<PrimitiveTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         [DateTimeFormat("yyyy-MM-dd HH:mm:ss.fff")]
+                         {{type}} Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+
+    [Theory]
+    [InlineData("TimeSpan")]
+    [InlineData("TimeSpan?")]
+    public void TimeSpanTest(string type)
+    {
+        var factory = new TestOutputLoggerFactory(testOutputHelper, LogLevel.Warning);
+        if (factory.CreateLogger<PrimitiveTypeTests>() is not TestOutputLogger<PrimitiveTypeTests> logger)
+        {
+            throw new InvalidOperationException("Logger creation failed.");
+        }
+
+        var code = $$"""
+                     [StaticDataRecord("Test", "TestSheet")]
+                     public sealed record MyRecord(
+                         [TimeSpanFormat("c")]
+                         {{type}} Property,
+                     );
+                     """;
+
+        var loadResult = RecordSchemaLoader.OnLoad(nameof(RecordTypeCheckerTest), code, logger);
+
+        var recordSchemaCollector = new RecordSchemaCollector(loadResult);
+        var recordSchemaContainer = new RecordSchemaContainer(recordSchemaCollector);
+        RecordComplianceChecker.Check(recordSchemaContainer, logger);
+
+        Assert.Empty(logger.Logs);
+    }
+}
