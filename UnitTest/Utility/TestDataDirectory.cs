525c22f898b22e85f744fcc6ea73dd167a5f29f5
강민석 <bluekms@shiftup.co.kr>
2026-05-20 11:52:05 +0900

    테스트 데이터를 UnitTest/TestData로 옮기고 임베디드 리소스로 추출하는 TestDataDirectory 추가

---
+++ UnitTest/Utility/TestDataDirectory.cs
@@ -0,0 +1,43 @@
+using System.Reflection;
+
+namespace UnitTest.Utility;
+
+internal sealed class TestDataDirectory : IDisposable
+{
+    private const string ResourceNamespace = "UnitTest.TestData";
+
+    public string Path { get; }
+
+    public TestDataDirectory(params string[] resourceFileNames)
+    {
+        Path = System.IO.Path.Combine(System.IO.Path.GetTempPath(), System.IO.Path.GetRandomFileName());
+        Directory.CreateDirectory(Path);
+
+        var assembly = Assembly.GetExecutingAssembly();
+        foreach (var fileName in resourceFileNames)
+        {
+            var resourceName = FormattableString.Invariant($"{ResourceNamespace}.{fileName}");
+            using var resourceStream = assembly.GetManifestResourceStream(resourceName);
+            if (resourceStream is null)
+            {
+                throw new InvalidOperationException(FormattableString.Invariant(
+                    $"Embedded resource not found: {resourceName}"));
+            }
+
+            var filePath = System.IO.Path.Combine(Path, fileName);
+            using var fileStream = File.Create(filePath);
+            resourceStream.CopyTo(fileStream);
+        }
+    }
+
+    public string GetFilePath(string fileName)
+        => System.IO.Path.Combine(Path, fileName);
+
+    public void Dispose()
+    {
+        if (Directory.Exists(Path))
+        {
+            Directory.Delete(Path, recursive: true);
+        }
+    }
+}
