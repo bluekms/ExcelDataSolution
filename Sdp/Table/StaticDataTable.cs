cb80af5e8eb05acfb0717182804973b0a0178275
강민석 <bluekms@shiftup.co.kr>
2026-06-13 02:49:55 +0900

    테이블의 리플렉션용 계약 인터페이스 제거

--- Sdp/Table/StaticDataTable.cs
+++ Sdp/Table/StaticDataTable.cs
@@ -1,10 +1,8 @@
-using System.Collections;
 using System.Collections.Immutable;
 
 namespace Sdp.Table;
 
 public abstract class StaticDataTable<TSelf, TRecord>(ImmutableArray<TRecord> records)
-    : IStaticDataTable
     where TSelf : StaticDataTable<TSelf, TRecord>
     where TRecord : notnull
 {
@@ -13,10 +11,4 @@ public abstract class StaticDataTable<TSelf, TRecord>(ImmutableArray<TRecord> re
     protected virtual void Validate()
     {
     }
-
-    Type IStaticDataTable.RecordType => typeof(TRecord);
-
-    IEnumerable IStaticDataTable.GetAllRecords() => records;
-
-    void IStaticDataTable.Validate() => Validate();
 }
