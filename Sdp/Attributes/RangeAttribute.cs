3aec91795f78868b7c330906db3ac2dc29642c1e
강민석 <bluekms@shiftup.co.kr>
2026-06-13 02:49:56 +0900

    Range·RegularExpression attribute의 DataAnnotations 상속 제거
    SG는 생성자 인자만 읽고 런타임도 base 기능을 쓰지 않아, netstandard2.1에 없는
    어셈블리 의존만 남기므로 독립 attribute로 전환한다.

--- Sdp/Attributes/RangeAttribute.cs
+++ Sdp/Attributes/RangeAttribute.cs
@@ -1,20 +1,32 @@
 namespace Sdp.Attributes;
 
+// DataAnnotations 상속은 netstandard2.1(유니티)에 없는 어셈블리 의존을 만들고,
+// SG 는 생성자 인자만 읽으므로 독립 attribute 로 충분하다.
 [AttributeUsage(AttributeTargets.Parameter)]
-public class RangeAttribute : System.ComponentModel.DataAnnotations.RangeAttribute
+public class RangeAttribute : Attribute
 {
     public RangeAttribute(double minimum, double maximum)
-        : base(minimum, maximum)
     {
+        Minimum = minimum;
+        Maximum = maximum;
     }
 
     public RangeAttribute(int minimum, int maximum)
-        : base(minimum, maximum)
     {
+        Minimum = minimum;
+        Maximum = maximum;
     }
 
     public RangeAttribute(Type type, string minimum, string maximum)
-        : base(type, minimum, maximum)
     {
+        OperandType = type;
+        Minimum = minimum;
+        Maximum = maximum;
     }
+
+    public object Minimum { get; }
+
+    public object Maximum { get; }
+
+    public Type? OperandType { get; }
 }
