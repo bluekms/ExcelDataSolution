718fca1a408417e995401332d9015186f3c6a088
강민석 <bluekms@shiftup.co.kr>
2026-06-15 19:24:09 +0900

    리뷰 - update ForbiddenColumnNameChars

--- SchemaInfoScanner/Schemata/SchemaValidators/ColumnNameAttributeValidator.cs
+++ SchemaInfoScanner/Schemata/SchemaValidators/ColumnNameAttributeValidator.cs
@@ -1,3 +1,4 @@
+using System.Buffers;
 using System.Globalization;
 using FluentValidation;
 using SchemaInfoScanner.Extensions;
@@ -8,9 +9,8 @@ namespace SchemaInfoScanner.Schemata.SchemaValidators;
 
 internal partial class SchemaRuleValidator
 {
-    // 컬럼명은 CSV 헤더로 그대로 직렬화되고, 중첩(.)·컬렉션([i]) 헤더 합성의 구분자로도 쓰인다.
-    // 이 문자들이 컬럼명에 들어가면 CSV 구조가 깨지거나 헤더 매핑이 어긋나므로 사전에 금지한다.
-    private static readonly char[] ForbiddenColumnNameChars = [',', '"', '.', '[', ']', '\n', '\r'];
+    private static readonly SearchValues<char> ForbiddenColumnNameChars =
+        SearchValues.Create([',', '"', '.', '[', ']', '\n', '\r']);
 
     private void RegisterColumnNameAttributeRule()
     {
@@ -31,5 +31,5 @@ internal partial class SchemaRuleValidator
         => property.GetAttributeValue<ColumnNameAttribute, string>(0);
 
     private static bool ContainsForbiddenCharacter(string columnName)
-        => columnName.IndexOfAny(ForbiddenColumnNameChars) != -1;
+        => columnName.AsSpan().ContainsAny(ForbiddenColumnNameChars);
 }
